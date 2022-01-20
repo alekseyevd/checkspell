6c02348b2662fdb884d2f848b6800be56776c009
alekseyevd <alekseyev.d@gmail.com>
2022-01-20 17:40:43 +0300

    fix

--- src/lib/http/bodyParser.ts
+++ src/lib/http/bodyParser.ts
@@ -3,24 +3,36 @@ import formidable from 'formidable'
 import fs from 'fs'
 import { IBodyParser } from './interfaces'
 
+async function fileHandler (file: any) {
+  return new Promise((resolve, reject) => {
+    const stream = fs.createWriteStream(file.filename)
+    file
+    //.on('end', () => resolve(file.filename))
+    .pipe(stream)
+    .on('finish', () => resolve(file.filename))
+  })
+
+  
+}
+
 const bodyParser: IBodyParser = async (req, options) => {
   const fileMeta: {[key:string] : any} = {}
-  let filesMeta: Array<any>
+  let filesMeta: Array<any> = []
 
   const form = formidable()
-  form.onPart = part => {
+  form.onPart = (part) => {
     if (!part.filename) {
       form.handlePart(part)
       return
     }
 
-    fileMeta[part.name] = {
-      name: part.name,
-      fileName: part.filename,
-      type: part.mime,
-      buffer: [],
-    }
-    const stream = fs.createWriteStream(part.filename)
+    // fileMeta[part.name] = {
+    //   name: part.name,
+    //   fileName: part.filename,
+    //   type: part.mime,
+    //   buffer: [],
+    // }
+    // const stream = fs.createWriteStream(part.filename)
 
     // part.on('data', function (buffer) {
     //   fileMeta[part.name].buffer.push(buffer)
@@ -31,16 +43,20 @@ const bodyParser: IBodyParser = async (req, options) => {
     //   filesMeta = Object.values(fileMeta)
 
     // })
-    part.pipe(stream)
+    //part.pipe(stream)
+    // fileMeta[part.name] = fileHandler(part)
+    filesMeta.push(fileHandler(part))
   }
 
   const { fields, files } = await new Promise((resolve, reject) => {
-    form.parse(req, (error, fields, files) => {
+    form.parse(req, async (error, fields) => {
       if (error) {
         reject(error)
         return
       }
-      resolve({ fields, files: fileMeta})
+  
+      const files = await Promise.all(filesMeta)
+      resolve({ fields, files})
       //resolve({ fields, files })
     })
   })
