acca820e9cb78e4ca45d4a8d9fbf7ca514bbb1e7
alekseyevd <alekseyev.d@gmail.com>
2022-01-22 16:07:00 +0300

    fix

--- src/lib/http/helpers/fileHandler.ts
+++ src/lib/http/helpers/fileHandler.ts
@@ -2,12 +2,13 @@ import fs from 'fs'
 import os from 'os'
 import path from 'path'
 import crypto from 'crypto'
+import stream from 'stream';
 
 const algorithm = 'aes-256-ctr';
 const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
 const iv = crypto.randomBytes(16);
 
-export default async function fileUploadHandler (file: any, params: any) {
+export default async function fileUploadHandler (file: stream.Readable, params: any) {
   if (!params.filename) throw new Error('Invalid filename...')
   return new Promise((resolve, reject) => {
     const encrypt = crypto.createCipheriv(algorithm, secretKey, iv);
