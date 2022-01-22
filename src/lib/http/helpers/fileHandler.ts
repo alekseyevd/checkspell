b1c694ec85a65402d00f6391e85187ca258e15ff
alekseyevd <alekseyev.d@gmail.com>
2022-01-22 16:04:48 +0300

    fix

--- src/lib/http/helpers/fileHandler.ts
+++ src/lib/http/helpers/fileHandler.ts
@@ -8,9 +8,8 @@ const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
 const iv = crypto.randomBytes(16);
 
 export default async function fileUploadHandler (file: any, params: any) {
+  if (!params.filename) throw new Error('Invalid filename...')
   return new Promise((resolve, reject) => {
-    if (!params.filename) reject(new Error('Invalid filename...'))
-
     const encrypt = crypto.createCipheriv(algorithm, secretKey, iv);
 
     const fileName = path.join(os.tmpdir(), `[${Date.now()}]_${params.filename}`)
