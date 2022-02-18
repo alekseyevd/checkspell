e74b79a72db7dcd1d3d3f65cf05e7383bd87bf29
alekseyevd <alekseyev.d@gmail.com>
2022-02-18 18:19:55 +0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,208 @@
-//console.log(Date.parse('sdfsf 2022'))
+'use strict';
 
-const map = new Map()
-map.set(1, 'dfgdfg')
-map.set(2, 'dfgdfg')
-console.log(Object.fromEntries(map))
+const debug = require('debug')('network');
+
+function Networker(socket, handler) {
+  this.socket = socket;
+  this._packet = {};
+  
+  this._process = false;
+  this._state = 'HEADER';
+  this._type = 0;
+  this._payloadLength = 0;
+  this._bufferedBytes = 0;
+  this.queue = [];
+
+  this.handler = handler;
+}
+
+Networker.prototype.init = function () {
+  this.socket.on('data', (data) => {
+    this._bufferedBytes += data.length;
+    this.queue.push(data);
+
+    this._process = true;
+    this._onData();
+  });
+
+  this.socket.on('served', this.handler);
+};
+
+Networker.prototype._hasEnough = function (size) {
+  if (this._bufferedBytes >= size) {
+    return true;
+  }
+  this._process = false;
+  return false;
+}
+
+Networker.prototype._readBytes = function (size) {
+  let result;
+  this._bufferedBytes -= size;
+
+  if (size === this.queue[0].length) {
+    return this.queue.shift();
+  }
+
+  if (size < this.queue[0].length) {
+    result = this.queue[0].slice(0, size);
+    this.queue[0] = this.queue[0].slice(size);
+    return result;
+  }
+  
+  result = Buffer.allocUnsafe(size);
+  let offset = 0;
+  let length;
+  
+  while (size > 0) {
+    length = this.queue[0].length;
+
+    if (size >= length) {
+      this.queue[0].copy(result, offset);
+      offset += length;
+      this.queue.shift();
+    } else {
+      this.queue[0].copy(result, offset, 0, size);
+      this.queue[0] = this.queue[0].slice(size);
+    }
+
+    size -= length;
+  }
+
+  return result;
+}
+
+Networker.prototype._getHeader = function () {
+  if (this._hasEnough(2)) {
+    this._payloadLength = this._readBytes(2).readUInt16BE(0, true);
+    this._state = 'TYPE';
+  }
+}
+
+Networker.prototype._getType = function () {
+  if (this._hasEnough(1)) {
+    this._type = this._readBytes(1).readUInt16BE(0, true);
+    this._state = 'ACTION';
+  }
+}
+
+Networker.prototype._getAction = function () {
+  if (this._hasEnough(1)) {
+    this._action = this._readBytes(1).readUInt16BE(0, true);
+    this._state = 'PAYLOAD';
+  }
+}
+
+Networker.prototype._getPayload = function () {
+  if (this._hasEnough(this._payloadLength)) {
+    //to do разделить на пакеты по 65536 байт
+    if (this._payloadLength > 65536) {
+      let n = (this._payloadLength / 65536).toFixed(0)
+      while (n) {
+        let received = this._readBytes(65536);
+        n--
+        this.socket.emit('packet', received)
+      }
+      let received = this._readBytes(this._payloadLength % 65536)
+      this.socket.emit('packetEnd', received)
+    }
+
+    let received = this._readBytes(this._payloadLength);
+    this.socket.emit('served', received);
+    this._state = 'HEADER';
+  }
+}
+
+Networker.prototype._onData = function (data) {
+  while (this._process) {
+    switch (this._state) {
+      case 'HEADER':
+        this._getHeader();
+        break;
+
+      case 'TYPE':
+        this._getType()
+        break;
+
+      case 'ACTION':
+        this._getAction()
+        break;
+
+      case 'PAYLOAD':
+        this._getPayload();
+        break;
+    }
+  }
+}
+
+Networker.prototype.send = function (message) {
+  let buffer = Buffer.from(message);
+  this._header(buffer.length);
+  this._packet.message = buffer;
+  this._send();
+}
+
+Networker.prototype._header = function (messageLength) {
+  this._packet.header = { length: messageLength };
+};
+
+Networker.prototype._send = function () {
+  let contentLength = Buffer.allocUnsafe(2);
+  contentLength.writeUInt16BE(this._packet.header.length);
+  debug('Attempting to write...', this._packet);
+  this.socket.write(contentLength);
+  this.socket.write(this._packet.message);
+  this._packet = {};
+};
+
+function isValidUTF8(buf) {
+  const len = buf.length;
+  let i = 0;
+
+  while (i < len) {
+    if ((buf[i] & 0x80) === 0x00) {  // 0xxxxxxx
+      i++;
+    } else if ((buf[i] & 0xe0) === 0xc0) {  // 110xxxxx 10xxxxxx
+      if (
+        i + 1 === len ||
+        (buf[i + 1] & 0xc0) !== 0x80 ||
+        (buf[i] & 0xfe) === 0xc0  // overlong
+      ) {
+        return false;
+      }
+
+      i += 2;
+    } else if ((buf[i] & 0xf0) === 0xe0) {  // 1110xxxx 10xxxxxx 10xxxxxx
+      if (
+        i + 2 >= len ||
+        (buf[i + 1] & 0xc0) !== 0x80 ||
+        (buf[i + 2] & 0xc0) !== 0x80 ||
+        buf[i] === 0xe0 && (buf[i + 1] & 0xe0) === 0x80 ||  // overlong
+        buf[i] === 0xed && (buf[i + 1] & 0xe0) === 0xa0  // surrogate (U+D800 - U+DFFF)
+      ) {
+        return false;
+      }
+
+      i += 3;
+    } else if ((buf[i] & 0xf8) === 0xf0) {  // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+      if (
+        i + 3 >= len ||
+        (buf[i + 1] & 0xc0) !== 0x80 ||
+        (buf[i + 2] & 0xc0) !== 0x80 ||
+        (buf[i + 3] & 0xc0) !== 0x80 ||
+        buf[i] === 0xf0 && (buf[i + 1] & 0xf0) === 0x80 ||  // overlong
+        buf[i] === 0xf4 && buf[i + 1] > 0x8f || buf[i] > 0xf4  // > U+10FFFF
+      ) {
+        return false;
+      }
+
+      i += 4;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = Networker;
